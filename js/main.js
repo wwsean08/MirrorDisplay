37a16dbcd84f6563f49d60f8f3a92407ed690083
Sean Smith <admin@wwsean08.com>
2015-12-30 19:03:59 -0800

    Stubbed out display for weather and stocks.  Initial Implementation of time and message.  Refresh on upgrade added.

--- js/main.js
+++ js/main.js
@@ -3,21 +3,91 @@
  */
 var ws = new SockJS('http://' + window.location.hostname + ':15674/stomp');
 var client = Stomp.over(ws);
+var user = 'guest';
+var pass = 'guest';
+var vhost = '/';
+var time_refresh = 1000;
 
 // SockJS does not support heart-beat: disable heart-beats
 client.heartbeat.outgoing = 0;
 client.heartbeat.incoming = 0;
 
-var on_message = function(data) {
-    var content = $('#content');
+/**
+ * A weather message has been sent, parse and display it
+ * TODO: Determine data structure and parse it to make it pretty
+ * @param data
+ */
+var on_message_weather = function(data) {
+    var content = $('#weather');
     content.text(data);
 }
+
+/**
+ * When a message is sent to this it means that an updated version of the
+ * application has been downloaded, refresh the page
+ */
+var on_message_update = function() {
+    window.location.reload();
+}
+
+/**
+ * A message......message has been sent, display it
+ * @param data
+ */
+var on_message_msg = function(data) {
+    var content = $('#message');
+    content.text(data);
+}
+
+/**
+ * A weather message has been sent, parse and display it
+ * TODO: Determine data structure and parse it to make it pretty
+ * @param data
+ */
+var on_message_stock = function(data) {
+    var content = $('#weather');
+    content.text(data);
+}
+
+
+/**
+ * Connect to RabbitMQ to get weather updates
+ * @param x
+ */
 var on_connect = function(x) {
-    id = client.subscribe("/topic/hello", function(d) {
-        on_message(d.body);
+    client.subscribe("/topic/weather", function (d) {
+        on_message_weather(d.body);
+    });
+    client.subscribe("/topic/message", function (d) {
+        on_message_msg(d.body);
+    });
+    client.subscribe("/topic/upgrade", function (_) {
+        on_message_update();
+    });
+    client.subscribe("/topic/stock", function (d) {
+        on_message_stock(d.body);
     });
 };
+
 var on_error =  function() {
     console.log('error');
 };
-client.connect('guest', 'guest', on_connect, on_error, '/')
+
+client.connect(user, pass, on_connect, on_error, vhost);
+
+function display_c(){
+    setTimeout('display_ct()',time_refresh);
+}
+
+function display_ct() {
+    var x = new Date();
+    var x1=x.getMonth() + "/" + x.getDate() + "/" + x.getYear();
+    x1 = x1 + " - " + x.getHours() + ":" + x.getMinutes();
+    $('#time').text(x1);
+
+    display_c();
+}
+
+window.onload = function() {
+    display_ct();
+};
